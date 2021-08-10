fbf3e9d75f7d007994f321d6094b646e8b92aa95
jens <jens_johansen1@hotmail.com>
2021-08-10 20:05:44 +0200

    demo firmware controls notes over uart

--- firmware/test.c
+++ firmware/test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "platform.h"
 #include "xil_printf.h"
 #include "xil_io.h"
@@ -7,11 +8,50 @@
 
 int main()
 {
+	uint32_t frequency = 440;
+
     init_platform();
 
+    Xil_Out32(REG_FILE_LEDS, 0x01);
     print("Hello World\n\r");
-    Xil_Out32(REG_LEDS, 0x01);
-    Xil_Out32(REG_UART_TX, 0x33);
+
+    while (1) {
+
+    	uint32_t slope = frequency << 16 / SAMPLE_RATE;
+    	Xil_Out32(REG_FILE_SAW_SLOPE, slope);
+
+        if (Xil_In32(REG_UART_STATUS) & 0x1) {
+
+            char recv_byte = Xil_In32(REG_UART_RX);
+            Xil_Out32(REG_UART_TX, recv_byte);
+
+            switch (recv_byte) {
+            	case 'a':
+            		frequency = 440;
+            		break;
+            	case 'b':
+            		frequency = 494;
+            		break;
+            	case 'c':
+            		frequency = 523;
+            		break;
+            	case 'd':
+            		frequency = 587;
+            		break;
+            	case 'e':
+            		frequency = 659;
+            		break;
+            	case 'f':
+            		frequency = 698;
+            		break;
+            	case 'g':
+            		frequency = 784;
+            		break;
+            	default:
+            		break;
+            }
+        }
+    }
 
     cleanup_platform();
     return 0;
--- firmware/wave_array.h
+++ firmware/wave_array.h
@@ -1,5 +1,10 @@
-#define REG_LEDS            0x40000000
-#define REG_UART_RX         0x40600000
-#define REG_UART_TX         0x40600004
-#define REG_UART_STATUS     0x40600008
-#define REG_UART_CONTROL    0x4060000C
+#define SAMPLE_RATE				48000
+#define REG_UART_BASE           0x40600000
+#define REG_UART_RX             (REG_UART_BASE + 0x0)
+#define REG_UART_TX             (REG_UART_BASE + 0x4)
+#define REG_UART_STATUS         (REG_UART_BASE + 0x8)
+#define REG_UART_CONTROL        (REG_UART_BASE + 0xC)
+#define REG_FILE_BASE           0x44A00000
+#define REG_FILE_SWITCHES       (REG_FILE_BASE + 0x0)
+#define REG_FILE_LEDS           (REG_FILE_BASE + 0x4)
+#define REG_FILE_SAW_SLOPE      (REG_FILE_BASE + 0x8)
