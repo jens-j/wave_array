12c5ea2ed39c502a30ca87a8e9bf51cb0166f3e2
jensj <j.johansen@sron.nl>
2021-12-16 17:36:32 +0100

    clk gen wrapper

--- vhdl/clk_generator_wrapper.vhd
+++ vhdl/clk_generator_wrapper.vhd
@@ -5,6 +5,7 @@ use IEEE.numeric_std.all;
 library wave;
 use wave.wave_array_pkg.all;
 
+
 -- Divides the I2S clock down by 4, further than the PLL can.
 entity clk_generator_wrapper is
     port (
@@ -26,23 +27,32 @@ architecture arch of clk_generator_wrapper is
         O: out std_logic);
     end component;
 
+    component clk_generator
+    port (
+        system_clk        : out    std_logic;
+        i2s_clk           : out    std_logic;
+        reset             : in     std_logic;
+        ext_clk           : in     std_logic
+    );
+    end component;
+
 begin
 
-    clk_gen : entity wave.clk_generator
+    clk_gen : clk_generator
     port map (
-        reset                   => reset,
-        ext_clk                 => ext_clk,
-        system_clk              => system_clk,
-        i2s_clk                 => i2s_intermediate_clk_s
+        system_clk => system_clk,
+        i2s_clk => i2s_intermediate_clk_s,
+        reset => reset,
+        ext_clk => ext_clk
+     );
+
+    clk_buffer : BUFG
+    port map(
+        I => counter_r(1),
+        O => i2s_clk
     );
 
-    -- clk_buffer : BUFG
-    -- port map(
-    --     I => counter_r(1),
-    --     O => i2s_clk
-    -- );
-
-    i2s_clk <= counter_r(1);
+    -- i2s_clk <= counter_r(1);
 
     divider_proc : process (i2s_intermediate_clk_s)
     begin
