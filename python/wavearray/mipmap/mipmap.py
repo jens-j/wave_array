f7664afde0e9225fee5cdef184e282d9ce0e2a94
jensj <j.johansen@sron.nl>
2022-04-30 23:10:05 +0200

    better filter in mipmap generation

--- python/wavearray/mipmap/mipmap.py
+++ python/wavearray/mipmap/mipmap.py
@@ -12,6 +12,8 @@ class Mipmap:
     SAMPLE_MIN = -2**15
     SAMPLE_MAX = 2**15 - 1
 
+    N_COEFF = 399 # Must be uneven.
+
 
     def __init__(self, name, waveform):
 
@@ -21,81 +23,85 @@ class Mipmap:
         self.waveform = waveform # Should be in range [-1, 1].
 
         self.spectra = [] # List of spectra for each mipmap level.
-        self.filtered_tables = [] # list of filtered waveforms for each mipmap level.
-        self.decimated_tables = [] # list of filtered and decimated waveforms for each mipmap level.
+        self.subtables = [] # List of mipmap subtables
 
         self.mipmap_table = self._create_table() # Flattened mipmap table.
 
 
     def _create_table(self):
 
-        mipmap_table = []
+        mipmap_table = np.array([])
         fft_size = self.L0_SIZE
 
-        self.spectra.append(np.fft.fft(self.waveform, fft_size) / self.SAMPLE_MAX / fft_size)
+        # Create halfband filter.
+        filter_coeffs = signal.remez(self.N_COEFF, [0, 0.490, 0.499, 0.5], [1, 0])
 
-        # print(self.waveform)
-        # print('sum(psd(f)) =', sum(np.abs(self.spectra[0])**2))
+        # w, h = signal.freqz(filter_coeffs, [1], worN=2000)
+        # fig, axis = plt.subplots(1)
+        # axis.plot(w / (2 * np.pi), 20 * np.log10(np.abs(h)))
 
-        # print(self.spectra[0])
+        # Add the unfiltered L0 spectrum.
+        self.subtables.append(self.waveform)
+        self.spectra.append(np.fft.rfft(self.waveform / self.SAMPLE_MAX, fft_size))
+
+        print(self.spectra)
 
         for level in range(0, self.L0_SIZE_LOG2 - 1):
 
-            # Filter above 1/2 Fs for L0. The cutoff is halved for each succesive table
-            self.spectra.append(self.spectra[0].copy())
-            self.spectra[level][int(np.ceil(self.L0_SIZE / (2 * 2**level)) + 1):] = 0+0j # 1E-9+1E-9j
+            offset = self.N_COEFF // 2
 
-            # Perform the IFFT of the modified spectrum to get the table.
-            self.filtered_tables.append(
-                np.fft.irfft(self.spectra[level], n=int(self.L0_SIZE)) * fft_size * self.SAMPLE_MAX)
-            self.decimated_tables.append(
-                np.fft.irfft(self.spectra[level], n=int(self.L0_SIZE / 2**level))
-                * fft_size * self.SAMPLE_MAX / 2**level)
+            # filter and downsample to create next subtable
+            filtered_table = np.convolve(self.subtables[-1], filter_coeffs)
+            self.subtables.append(filtered_table[offset:-offset:2])
+            self.spectra.append(np.fft.rfft(
+                self.subtables[-1] / self.SAMPLE_MAX, 2**(self.L0_SIZE_LOG2 - level)))
 
-            # # Normalize the table
-            # scale = self.SAMPLE_MAX / np.max(np.abs(self.decimated_tables[-1]))
-            # self.decimated_tables[-1] *= scale
 
-            mipmap_table.extend(self.decimated_tables[-1])
+            mipmap_table = np.append(mipmap_table, self.subtables[-1])
 
         # Pad mipmap with zero's to power of two
         pad_length = 2**self.L0_SIZE_LOG2 * 2 - len(mipmap_table)
-        mipmap_table.extend([0] * pad_length)
+        mipmap_table = np.append(mipmap_table, [0] * pad_length)
 
         return mipmap_table
 
 
     def plot(self):
 
-        fig, axes = plt.subplots(self.L0_SIZE_LOG2 - 1, 3)
+        fig, axes = plt.subplots(self.L0_SIZE_LOG2, 2)
         fig.suptitle(self.name)
 
         spectrum_x = np.array(range(self.L0_SIZE // 2 + 1))
 
-        for level in range(0, self.L0_SIZE_LOG2 - 1):
+        print(self.spectra)
+
+        for level in range(0, self.L0_SIZE_LOG2):
 
             # spectrum = np.abs(self.spectra[level])
-            spectrum = 20 * np.log10(np.abs(self.spectra[level]) / self.SAMPLE_MAX)
+            spectrum = 20 * np.log10(np.abs(self.spectra[level])**2)
 
-            # Plot tables
+            # Plot spectra
             axes[level, 0].set_title('L{} spectrum'.format(level))
 
-            axes[level, 0].plot(spectrum_x[:(self.L0_SIZE // 2**(level + 1) + 1)],
-                                spectrum[:(self.L0_SIZE // 2**(level + 1) + 1)])
+            axes[level, 0].plot(spectrum)
+            axes[level, 0].plot(spectrum, 'ro', markersize=2)
 
-            axes[level, 0].plot(spectrum_x[:(self.L0_SIZE // 2**(level + 1) + 1)],
-                                spectrum[:(self.L0_SIZE // 2**(level + 1) + 1)],
-                                'ro', markersize=2)
+            # axes[level, 0].plot(spectrum_x[:(self.L0_SIZE // 2**(level + 1) + 1)],
+            #                     spectrum[:(self.L0_SIZE // 2**(level + 1) + 1)])
+            #
+            # axes[level, 0].plot(spectrum_x[:(self.L0_SIZE // 2**(level + 1) + 1)],
+            #                     spectrum[:(self.L0_SIZE // 2**(level + 1) + 1)],
+            #                     'ro', markersize=2)
 
             # axes[level, 0].set_yscale("log");
             # axes[level, 0].plot(spectrum_x[:8], np.abs(self.spectra[level][:8] / self.L0_SIZE)**2)#, 'ro', markersize=2)
 
-            axes[level, 1].set_title('L{} waveform'.format(level))
-            axes[level, 1].plot(self.filtered_tables[level])
+            # axes[level, 1].set_title('L{} waveform'.format(level))
+            # axes[level, 1].plot(self.filtered_tables[level])
 
-            axes[level, 2].set_title('L{} table'.format(level))
-            axes[level, 2].plot(self.decimated_tables[level])
-            axes[level, 2].plot(self.decimated_tables[level], 'ro', markersize=2)
+            axes[level, 1].set_title('L{} table'.format(level))
+            axes[level, 1].plot(self.subtables[level])
+            axes[level, 1].plot(self.subtables[level], 'ro', markersize=2)
 
         plt.show()
 
@@ -119,7 +125,7 @@ def main():
 
     acid = read("Acid.wav")[1][:2048]
 
-    mm = Mipmap('square', square)
+    mm = Mipmap('acid', acid)
     mm.plot()
     mm.write_table()
 
