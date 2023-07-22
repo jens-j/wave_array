98755f365bc77b9a7b0b1288636f766d9f495c39
jensj <j.johansen@sron.nl>
2023-07-22 18:48:01 +0200

    wavetable class

---
+++ python/wave_array/client/wavetable.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class WaveTable:
+
+    def __init__(self):
+        
+        self.n_frames = 0
+        self.n_frames_log2 = 0
+        self.frames = []
+
+
+    def initialize(self, data):
+
+        assert len(data) % 4096 == 0, f'Invalid wavetable length {len(data)}'
+
+        self.n_frames = len(data) // 4096
+        self.n_frames_log2 = int(np.log2(self.n_frames))
+
+        for i in range(self.n_frames):
+            self.frames.append(data[i * 4096:i * 4096 + 2048])
+
